a600f5ecb2c3d67c90b72fb5490a4f2d50671e5d
A RAJU <avularaju9866@achiever-115.local>
2019-11-26 10:15:20 +0530

    Initial Commit

---
+++ datapassing through appdelegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  datapassing through appdelegate
+//
+//  Created by A RAJU on 11/26/19.
+//  Copyright © 2019 A RAJU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
